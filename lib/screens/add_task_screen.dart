5c7f5a8c2a4ad57ee7e7226487671b3cce255da1
Heriberto-Juarez <heribertojuarezj@gmail.com>
2024-04-16 07:02:59 -0600

    Manage the state of the tasks checkboxes

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -16,38 +16,36 @@ class AddTaskScreen extends StatelessWidget {
               topRight: Radius.circular(20.0),
             ),
         ),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                  "Add Task",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    color: Colors.lightBlueAccent,
-                  ),
-              ),
-              const TextField(
-                autofocus: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+                "Add Task",
                 textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.lightBlueAccent,
-                  backgroundColor: Colors.lightBlueAccent,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.lightBlueAccent,
                 ),
-                onPressed: () {
+            ),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.lightBlueAccent,
+                backgroundColor: Colors.lightBlueAccent,
+              ),
+              onPressed: () {
 
-                }, child: const Text("Add", style: TextStyle(
-                  color: Colors.white
-              ),),
-              )
-            ],
-          ),
+              }, child: const Text("Add", style: TextStyle(
+                color: Colors.white
+            ),),
+            )
+          ],
         ),
       ),
     );
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+
+  bool isChecked = false;
+
+  void checkboxCallback (bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("This is a task"),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value){
-
-        },
+      title: Text(
+          "This is a task",
+          style: TextStyle(decoration: (isChecked ? TextDecoration.lineThrough : null)),
       ),
+      trailing: TaskCheckbox(checkboxState: isChecked,toggleCheckboxState: checkboxCallback,),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+  const TaskCheckbox({super.key, required this.checkboxState, required this.toggleCheckboxState});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (newValue) {
+        toggleCheckboxState(newValue);
+      },
     );
   }
 }
